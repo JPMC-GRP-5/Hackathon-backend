0c056b3d674515aecd0a30b449da4d2515ac79b7
Rupin <rupinmalik15@gmail.com>
2023-03-16 23:35:18 +0530

    solved conflicts

--- src/app.js
+++ src/app.js
@@ -5,13 +5,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import UserRouter from "./routes/User.js";
 import HotelRouter from "./routes/Hotel.js";
-<<<<<<< HEAD
 import Iternary from './routes/Iternary.js'
 import CitiesRouter from './routes/Cities.js'
-=======
 import PlaceRouter from "./routes/Places.js";
 
->>>>>>> 86a7f70da605cccaa8d70a26c3c091f88ec906f9
 const PORT = process.env.PORT || 8081;
 
 const app = express();
@@ -30,9 +27,4 @@ app.use(bodyParser.json());
 
 app.use("/users", UserRouter);
 app.use("/hotels", HotelRouter);
-<<<<<<< HEAD
-app.use("/iternary",Iternary);
-app.use("/cities",CitiesRouter)
-=======
 app.use("/places", PlaceRouter);
->>>>>>> 86a7f70da605cccaa8d70a26c3c091f88ec906f9
